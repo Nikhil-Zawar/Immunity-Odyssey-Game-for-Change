fc58997236e56b86303d40368e9a2aad02e3a100
Nikhil-Zawar <157366906+Nikhil-Zawar@users.noreply.github.com>
2024-04-19 09:39:27 +0530

    Add files via upload

---
+++ all_data.py
@@ -0,0 +1,49 @@
+all_info = {
+	'diabetes' : {
+        "Level1" : {
+            "Point1" : "Diabetes is a condition that happens when your blood sugar is too high.",
+            "Point2" : "It develops when the pancreas doesn\'t make enough(or no) insulin to digest the sugars.",
+            "Point3" : "There are multiple types of diabetes - majorly Type1 and Type2 diabetes.",
+            "Point4" : "Both Type 1 and Type 2 diabetes can be influenced by genetics, means inherited.",
+            "Point5" : "Apart from genetics, a sedentary life, poor diet, excessive body fat can cause diabetes.",
+            "Point6" : "Chronic high blood sugar level can cause problems like heart disease, nerve damages and eye issues."
+        },
+        "Level2" : {
+            "Intro" : "Welcome to Level 2!",
+            "Objective": "In this level you will know about \"Type 2 Diabetes\" - Most Common Type",
+            "Point1" : "This is the most common type of diabetes, where the body doesn't make enough insulin.",
+            "Point2" : "This mainly affects adults. The body cells don't respond to insulin.",
+            "Point3" : "Without treatment - it can cause heart disease, kidney diesease and stroke.",
+            "Point4" : "Risk Factors for Type2 diabetes : Overweight, Obese, High BP, High Cholestrol.",
+            "Point5" : "Management - More exercises, Eat variety of nutritious food, Blood sugar monitoring.",
+            "Point6" : "With lifestyle changes this disease can be reversed to a large extent.",
+        }
+    }
+}
+
+
+all_quiz = {
+    "diabetes" : {
+        'Level1' : {
+            "Question" : "What points from below are correct about Type2 diabetes:",
+            "Option1" : "1. Diabetes is a condition that occurs when your blood sugar is too low.",
+            "Option2" : "2. Chronic high blood sugar levels cause complications such as heart disease.",
+            "Option3" : "3. It can affect the eyes, leading to conditions like diabetic retinopathy.",
+            "Option4" : "4. Pancreatitis or removal of part or all of the pancreas, can lead to diabetes.",
+        },
+        'Level2' : {
+            "Question" : "What points from below are correct about Type2 diabetes:",
+            "Option1": "1. Having Overweight does not increase risk towards diabetes.",
+            "Option2": "2. Type2 diabetes happens only by heredity of the same condition.",
+            "Option3": "3. During this, you feel more thirsty and hungrier than usual.",
+            "Option4": "4. In Type2 diabetes, the pancreas do not produce enough insulin.",
+        }
+    }
+}
+
+all_answer = {
+    "diabetes" : {
+        'Level1' : [False, True, True, True],
+        'Level2' : [False, False, True, True]
+    }
+}
